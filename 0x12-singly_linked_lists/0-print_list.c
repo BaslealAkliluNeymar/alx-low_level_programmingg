4150a6f9194c3f3caebb8e01a0d9687a3731ee07
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2022-05-19 05:17:29 -0700

    dfsd

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints elemnts of the linked list
+ * @h: pointer to the list_t list
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+size_t t = 0;
+while(h)
+{
+if(!h->str){
+printf("[0] (nil)");
+}
+else {
+printf("[%u] %s\n" , h->len , h->str);
+}
+t++;
+h = h->next;
+}
+return (t);
+}
