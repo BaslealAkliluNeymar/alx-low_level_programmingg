778568bdb51ae720f01343c606e9eef42ce902f4
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2022-05-26 08:53:34 -0700

    dsafeafe

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,20 +6,20 @@
  *
  * Return: the converted number
  */
-
 unsigned int binary_to_uint(const char *b)
 {
+int i;
 unsigned int val = 0;
-int i = 0;
 
-if (b == NULL)
-return 0;
+if (!b)
+return (0);
 
-while (b[i] == '0' || b[i] == '1')
-{  // Found another digit.
-val <<= 1;
-val += b[i]-'0';
-i++;
+for (i = 0; b[i]; i++)
+{
+if (b[i] < '0' || b[i] > '1')
+return (0);
+val = 2 * val + (b[i] - '0');
 }
-return val;
+
+return (val);
 }
